fc631dcc297629d06cb6979c93790c182ab567f4
Warren Hyson <84579627+LearnThinkCreate@users.noreply.github.com>
2024-02-25 10:27:48 -0500

    fix tailwind config

--- ui/Dashboard.tsx
+++ ui/Dashboard.tsx
@@ -8,38 +8,53 @@ export default async function Dashboard() {
   return (
 
     <>
-      <div className="grid grid-cols-3 gap-4">
-        <div className="">
-          <Suspense fallback={<div>Loading...</div>}>
+      <div className="flex flex-row gap-5">
+        <div className="basis-1/3 flex-none">
+          {/* <Suspense fallback={<div>Loading...</div>}> */}
             <DataTableWrapper defaultDropdownValue="main_genre" />
-          </Suspense>
+          {/* </Suspense> */}
         </div>
-        <div className="col-span-2 ...">
-          <Suspense fallback={<div>Loading...</div>}>
+        <div className="basis-2/3">
+          {/* <Suspense fallback={<div>Loading...</div>}> */}
             <LineChartWrapper
               height={430}
               chartId="test"
               defaultDropdownValue="main_genre"
             />
-          </Suspense>
+          {/* </Suspense> */}
         </div>
-        <div >
-          <Suspense fallback={<div>Loading...</div>}>
+      </div>
+
+    <br />
+      
+      <div className="grid grid-col-12 grid-flow-col gap-4 justify-stretch">
+        <div
+        //  className="col-start-1 col-end-3"
+        className="col-span-5"
+         >
+          {/* <Suspense fallback={<div>Loading...</div>}> */}
+          {/* <div className="flex flex-grow"> */}
             <BarChartWrapper
-              height={450}
+             
               chartId="test"
-              defaultDropdownValue="artist"
+              defaultDropdownValue="song"
+              classNames=""
             />
-          </Suspense>
+          {/* </div> */}
+          {/* </Suspense> */}
         </div>
-        <div className="col-span-2 ...">
-          <Suspense fallback={<div>Loading...</div>}>
+        
+        <div
+        //  className="container col-start-3 col-end-4"
+        className="col-span-7"
+         >
+          {/* <Suspense fallback={<div>Loading...</div>}> */}
             <LineChartWrapper
-              height={450}
+              
               chartId="test"
               defaultDropdownValue="artist"
             />
-          </Suspense>
+          {/* </Suspense> */}
         </div>
       </div>
     </>
