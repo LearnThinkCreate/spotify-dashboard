a1526e648548cb0f6fb1193b57929452ce4e06d2
Warren Hyson <84579627+LearnThinkCreate@users.noreply.github.com>
2024-04-05 22:54:58 -0400

    clean barGraph page

--- app/barGraph/page.tsx
+++ app/barGraph/page.tsx
@@ -1,21 +1,7 @@
 import * as React from "react";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-} from "@/components/ui/card";
-import GraphDropDown from "@/components/graphics/graph-dropdown";
-import  { BarGraphOptions } from "@/components/graphics/options";
 import { EraFilter } from "@/components/era-filter";
 import { ThemeWrapper } from "@/components/theme-wrapper";
-import HistoryBarChart from "@/components/graphics/bar-chart";
-import { GenreSearch } from "@/components/genre-search";
-import { GenreBadges } from "@/components/genre-badges";
-import { themes, Theme } from "@/components/themes";
 import prisma from "@/lib/db/prisma";
-import { prismaGenreFilters, prismaGenreOptions, eraFilters } from "@/lib/navigation-utils";
 import { BarGraph } from "@/components/graphics/bar-graph";
 
 export default async function Page({}) {
