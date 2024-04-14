e291464a7b136b900ab37c6fa5acd4cadddcce18
Warren Hyson <84579627+LearnThinkCreate@users.noreply.github.com>
2024-04-14 11:35:46 -0400

    format

--- components/graph-pie.tsx
+++ components/graph-pie.tsx
@@ -5,7 +5,6 @@ import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
 import { cn } from "@/lib/utils";
 import { useMediaQuery } from "@/hooks/use-media-query";
 import { useScreenWidth } from "@/hooks/screen-width";
-import * as recharts from "recharts";
 
 export const PieGraph = ({
   data,
@@ -20,13 +19,16 @@ export const PieGraph = ({
 }) => {
   const { currentTheme, themeCodes } = useThemeState();
 
-
   const isXlDesktop = useMediaQuery("(min-width: 1536px)");
   const isDesktop = useMediaQuery("(min-width: 1024px)");
   const isMini = useMediaQuery("(max-width: 400px)");
   const screenWidth = useScreenWidth();
 
-  const { innerRadius, outerRadius } = getRadiusSize({ screenWidth, isDesktop, isXlDesktop });
+  const { innerRadius, outerRadius } = getRadiusSize({
+    screenWidth,
+    isDesktop,
+    isXlDesktop,
+  });
 
   const secondaryColor =
     currentTheme?.era !== ""
@@ -40,42 +42,26 @@ export const PieGraph = ({
   }
 
   return (
-    <ResponsiveContainer 
-      width="100%" 
-      height="100%" 
+    <ResponsiveContainer
+      width="100%"
+      height="100%"
       minHeight={isDesktop ? undefined : 200}
     >
-      <PieChart
-      // margin={{ top: 5, right: 30, bottom: 5, left: 30 }}
-      >
+      <PieChart>
         <Pie
           data={data}
           cx="50%"
           cy="75%"
-          // outerRadius={80}
-          // paddingAngle={0}
-          // fill="#8884d8"
           dataKey={dataKey as string}
           nameKey={nameKey}
-          // label={({ name, percent }) => {
-          //   return `${name} ${(percent * 100).toFixed(0)}%`;
-          // }}
-
-          // @ts-ignore
-          label={<PieLabel isMini={isMini} isDesktop={isDesktop}/>}
-          // label={<TestLabel/>}
-          // data={data}
-          // cx={420}
-          // cy={200}
           startAngle={180}
           endAngle={0}
-          // innerRadius={30}
-          // outerRadius={40}
           innerRadius={innerRadius}
           outerRadius={outerRadius}
           fill="#8884d8"
           paddingAngle={5}
-          // dataKey="value"
+          // @ts-ignore
+          label={<PieLabel isMini={isMini} isDesktop={isDesktop} />}
         >
           {data.map((entry, index) => (
             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
@@ -92,29 +78,26 @@ const calculateSize = ({
   maxScreenWidth,
   minScreenWidth,
   screenWidth,
-  steps=10
-}) => (
+  steps = 10,
+}) =>
   maxMultiplier -
   ((maxMultiplier - minMultiplier) / steps) *
     (steps / (maxScreenWidth - minScreenWidth)) *
-    (maxScreenWidth - screenWidth)
-  
-)
-
+    (maxScreenWidth - screenWidth);
 
 const getRadiusSize = ({ screenWidth, isDesktop, isXlDesktop }) => {
   if (!isDesktop) {
     return {
       innerRadius: 60,
       outerRadius: 80,
-    }
+    };
   }
 
   if (isXlDesktop) {
     return {
       innerRadius: 52.5,
       outerRadius: 70,
-    }
+    };
   }
 
   const largeDesktop = {
@@ -122,28 +105,31 @@ const getRadiusSize = ({ screenWidth, isDesktop, isXlDesktop }) => {
     maxWidth: 1279,
     minOuterRadius: 40,
     maxOuterRadius: 60,
-  }
+  };
 
   const xlDesktop = {
     minWidth: 1280,
     maxWidth: 1535,
     minOuterRadius: 40,
     maxOuterRadius: 60,
-  }
+  };
 
-  if (screenWidth >= largeDesktop.minWidth && screenWidth <= largeDesktop.maxWidth) {
+  if (
+    screenWidth >= largeDesktop.minWidth &&
+    screenWidth <= largeDesktop.maxWidth
+  ) {
     const outerRadius = calculateSize({
       maxMultiplier: largeDesktop.maxOuterRadius,
       minMultiplier: largeDesktop.minOuterRadius,
       maxScreenWidth: largeDesktop.maxWidth,
       minScreenWidth: largeDesktop.minWidth,
       screenWidth,
-      steps: 10
+      steps: 10,
     });
     return {
-      innerRadius: (3/4) * outerRadius,
+      innerRadius: (3 / 4) * outerRadius,
       outerRadius,
-    }
+    };
   }
 
   const outerRadius = calculateSize({
@@ -152,22 +138,14 @@ const getRadiusSize = ({ screenWidth, isDesktop, isXlDesktop }) => {
     maxScreenWidth: xlDesktop.maxWidth,
     minScreenWidth: xlDesktop.minWidth,
     screenWidth,
-    steps: 10
+    steps: 10,
   });
 
   return {
-    innerRadius: (3/4) * outerRadius,
+    innerRadius: (3 / 4) * outerRadius,
     outerRadius,
-  }
-
+  };
 };
-
-
-const TestLabel = ({...props}: recharts.PieLabelRenderProps) => {
-  console.log(props);
-  return <></>;
-}
-
 
 const PieLabel = ({
   x,
@@ -183,7 +161,7 @@ const PieLabel = ({
   maxRadius,
   isMini,
   isDesktop,
-  middleRadius
+  middleRadius,
 }) => {
   let fontSize;
 
@@ -208,22 +186,19 @@ const PieLabel = ({
   let nameDelta = 0;
   let nameLength = name.length * charWidth;
   if (x < cx) {
-    if ((x + 5) -  nameLength < 0) {
-
+    if (x + 5 - nameLength < 0) {
       nameDelta = nameLength - x;
     }
   } else {
-    if (
-      (nameLength + x + defaultDelta) > (cx * 2)
-    ) {
-      nameDelta = ((nameLength + x + defaultDelta) - (cx * 2)) * -1;
+    if (nameLength + x + defaultDelta > cx * 2) {
+      nameDelta = (nameLength + x + defaultDelta - cx * 2) * -1;
     }
   }
 
   return (
     <>
-      <PieLabelText 
-        word={name as string} 
+      <PieLabelText
+        word={name as string}
         ydelta={-20}
         x={x}
         y={y}
@@ -245,16 +220,15 @@ const PieLabel = ({
   );
 };
 
-
 const PieLabelText = ({
   word,
   ydelta = 0,
   xdelta = 0,
-  x=0,
-  y=0,
-  cx=0,
-  fill="",
-  fontSize=14
+  x = 0,
+  y = 0,
+  cx = 0,
+  fill = "",
+  fontSize = 14,
 }: {
   word: string;
   ydelta?: number;
@@ -278,3 +252,8 @@ const PieLabelText = ({
     </text>
   </>
 );
+
+// const TestLabel = ({...props}: recharts.PieLabelRenderProps) => {
+//   console.log(props);
+//   return <></>;
+// }
