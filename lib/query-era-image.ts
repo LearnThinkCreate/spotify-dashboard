9e65b3dcceb8a3b42c8de66d4404c88aea88be8d
Warren Hyson <84579627+LearnThinkCreate@users.noreply.github.com>
2024-04-15 23:33:40 -0400

    diff path

--- lib/query-era-image.ts
+++ lib/query-era-image.ts
@@ -16,17 +16,31 @@ export const getRandomImagePath = async (era?: Theme) => {
             ? era.imagePath
             : subDirs[Math.floor(Math.random() * subDirs.length)];
       process.env.pu
-      const dirPath = path.join(process.cwd(), "public", "images", subDir);
+      const dirPath = path.join(process.cwd(), "public/images", subDir);
       const originalPath = dirPath.replace("./public", "");
       const newPath = path.resolve(originalPath);
-      const files = fs.readdirSync(newPath);
-      const imageFiles = files.filter((file) =>
-         /\.(jpg|jpeg|png|gif)$/i.test(file)
-      );
-      const randomImage =
-         imageFiles[Math.floor(Math.random() * imageFiles.length)];
-      const imagePath = `/images/${subDir}/${randomImage}`;
-      return imagePath;
+      try {
+         fs.accessSync(newPath);
+
+      }
+      catch (e) {
+         console.log("Error: ", e)
+      }
+      try {
+         const files = fs.readdirSync(newPath);
+         const imageFiles = files.filter((file) =>
+            /\.(jpg|jpeg|png|gif)$/i.test(file)
+         );
+         const randomImage =
+            imageFiles[Math.floor(Math.random() * imageFiles.length)];
+         const imagePath = `/images/${subDir}/${randomImage}`;
+         return imagePath;
+      }
+      catch (e) {
+         console.log("Error: ", e)
+         return "/images/hs/2E659CA0-9183-4D48-ACC5-1D51885CB809.jpeg.jpg"
+      }
+
    }
 
    return {
