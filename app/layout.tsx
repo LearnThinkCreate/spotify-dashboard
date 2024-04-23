7c1864c8ac9e3708ad4ade16254d071eae801c43
Warren Hyson <84579627+LearnThinkCreate@users.noreply.github.com>
2024-04-23 11:40:04 -0400

    add analytics componenet

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,49 +1,47 @@
 import "./globals.css";
-import "@/public/registry/themes.css"
-import { fontSans, fontMono } from "@/lib/fonts"
+import "@/public/registry/themes.css";
+import { fontSans, fontMono } from "@/lib/fonts";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ThemeProvider } from "@/components/theme-provider";
-import { ThemeSwitcher } from "@/components/theme-switcher"
+import { ThemeSwitcher } from "@/components/theme-switcher";
 import { cn } from "@/lib/utils";
 import { TailwindIndicator } from "@/components/tailwind-indicator";
 import { EraFilter } from "@/components/nav-era-filter";
-import { Analytics } from "@vercel/analytics/react"
-
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({
-  children
+   children,
 }: {
-  children: React.ReactNode;
+   children: React.ReactNode;
 }) {
-  return (
-    <html suppressHydrationWarning lang="en">
-      <head />
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontMono.className
-        )}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-        <div className="flex flex-col h-svh lg:h-lvh overflow-hidden">
-            <div className="sticky top-0 z-50 shadow px-4 py-2 w-full">
-                {/* <MenuBar /> */}
-                <EraFilter />
-              </div>
-              <main className="flex-1 overflow-hidden">
-                {children}
-              </main>
-        </div>
-          <TailwindIndicator />
-          <SpeedInsights />
-          <ThemeSwitcher />
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+   return (
+      <html suppressHydrationWarning lang="en">
+         <head />
+         <body
+            className={cn(
+               "min-h-screen bg-background font-sans antialiased",
+               fontMono.className
+            )}
+         >
+            <ThemeProvider
+               attribute="class"
+               defaultTheme="dark"
+               enableSystem
+               disableTransitionOnChange
+            >
+               <div className="flex flex-col h-svh lg:h-lvh overflow-hidden">
+                  <div className="sticky top-0 z-50 shadow px-4 py-2 w-full">
+                     {/* <MenuBar /> */}
+                     <EraFilter />
+                  </div>
+                  <main className="flex-1 overflow-hidden">{children}</main>
+               </div>
+               <Analytics />
+               <TailwindIndicator />
+               <SpeedInsights />
+               <ThemeSwitcher />
+            </ThemeProvider>
+         </body>
+      </html>
+   );
 }
